ad80a22277e38aa4e46bab44c5382a6b7deca6a9
yoga-adi-saputra <visectedbdm01@gmail.com>
2021-10-11 18:54:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum_fragment/fragment2.java
@@ -0,0 +1,14 @@
+package com.example.praktikum_fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater; import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment2 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+}
